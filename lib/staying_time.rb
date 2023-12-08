58d2d0e4d4b6950806398c48ebdbf235ffc02786
Aki Tada <3615622+torinoko@users.noreply.github.com>
2023-12-08 19:49:10 +0900

    インデントを修正

--- lib/staying_time.rb
+++ lib/staying_time.rb
@@ -1,12 +1,12 @@
 class StayingTime
-    def initialize(enter_time, number)
-        @enter_time = enter_time
-        @number = number
-    end
+  def initialize(enter_time, number)
+    @enter_time = enter_time
+    @number = number
+  end
 
-    def price(exit_time)
-        total_time = exit_time - @enter_time
-        total_time * @number
-    end
+  def price(exit_time)
+    total_time = exit_time - @enter_time
+    total_time * @number
+  end
 end
 

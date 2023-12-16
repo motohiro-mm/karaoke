4ca48f8927648f61d756096beac1be72504ae3f9
motohiro-mm <polarbear.dx@gmail.com>
2023-12-16 20:57:09 +0900

    滞在時間を小数点以下繰り上げに修正

--- lib/staying_time.rb
+++ lib/staying_time.rb
@@ -7,8 +7,8 @@ class StayingTime
   end
 
   def price(exit_time)
-    total_time = (Time.parse(exit_time) - Time.parse(@enter_time)) / 3600
-    (total_time * @number * 150).round
+    total_time = ((Time.parse(exit_time) - Time.parse(@enter_time)) / 3600).ceil
+    total_time * @number * 150
   end
 end
 

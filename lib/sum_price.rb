5e06c4331f46e1b660d749bc96c98d958d98e2b9
motohiro-mm <polarbear.dx@gmail.com>
2023-12-16 21:07:34 +0900

    SumPriceクラスのinitializeメソッドを整理した

--- lib/sum_price.rb
+++ lib/sum_price.rb
@@ -1,26 +1,25 @@
 class SumPrice
   def initialize(data)
-    @drink_price = 
+    @drink = 
       if data[0].include?("free_drink")
-        drink = FreeDrink.new(data[0][4])
-        drink.price
+        FreeDrink.new(data[0][4])
       elsif data[0].include?("one_drink")
-        drink = OneDrink.new(data[1][2], data[1][3])
-        drink.price
+        OneDrink.new(data[1][2], data[1][3])
       end
 
-    if data[0].include?("time_base")
-      time_table = StayingTime.new(data[0][0], data[2][0], data[0][4])
-      @time_price = time_table.price
-    elsif data[0].include?("free_time")
-      free_time = FreeTime.new(data[0][4])
-      @time_price = free_time.price
-    end
+    @time =
+      if data[0].include?("time_base")
+        StayingTime.new(data[0][0], data[2][0], data[0][4])
+      elsif data[0].include?("free_time")
+        FreeTime.new(data[0][4])
+      end
   end
 
   def total
-    puts "基本料金: #{@time_price}円"
-    puts "飲み物代: #{@drink_price}円"
-    puts "合計金額: #{@drink_price + @time_price}円"
+    time_price = @time.price
+    drink_price = @drink.price
+    puts "基本料金: #{time_price}円"
+    puts "飲み物代: #{drink_price}円"
+    puts "合計金額: #{time_price + drink_price}円"
   end
 end
